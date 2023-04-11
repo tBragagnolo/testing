9055064f0266c2c5a1cc015def6da40699ab3cf4
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-04-11 10:28:36 -0400

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <br /><Component {...pageProps} />
+  return <><br /><Component {...pageProps} /></>
 }
 
 export default MyApp
